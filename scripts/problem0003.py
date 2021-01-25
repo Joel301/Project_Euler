15d12541a3489fa2437fdfba416871d85edc9101
unknown <joel3.1416@gmail.com>
2021-01-25 14:53:19 -0600

    problem 4 solved

--- scripts/problem0003.py
+++ scripts/problem0003.py
@@ -10,22 +10,22 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?
 """
 
-def divisibles(number= 13195):
+def primeFactors(number= 13195):
 	p=2
 	while number >= p*p:
 		if number % p == 0:
-			print(p,", ")
+			yield p
 			number = number/p
 		else:
 			p=p+1
-	print(number)
+	yield int(number)
+	return
 
-#main function description
+#main function
 def compute():
-	ans = sum(x for x in range(1000) if (x % 3 == 0 or x % 5 == 0))
+	ans = [x for x in primeFactors(600851475143)]
 	return str(ans)
 
 
 if __name__ == "__main__":
-	#print(compute())
-	print(divisibles())
+	print(compute())
---
+++ scripts/problem0004.py
@@ -0,0 +1,30 @@
+
+#! python3
+# -*- coding: utf-8 -*-
+
+"""
+Euler description from https://projecteuler.net/
+Problem 0004
+A palindromic number reads the same both ways. The largest palindrome made from 
+the product of two 2-digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+"""
+
+
+def largestPalindrome(digits=2):
+    start = int('1'.ljust(digits, '0'))
+    n1, n2 = start, start
+    ans = 0
+    while n1 <= start*10:
+        while n2 <= start*10:
+            if str(n1*n2) == str(n1*n2)[::-1]:
+                if ans < n1*n2:
+                    ans = n1*n2
+            n2 += 1
+        n1 += 1
+        n2 = start
+    return ans
+
+if __name__ == "__main__":
+    print(largestPalindrome(3))
