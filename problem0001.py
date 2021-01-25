a693b9354b6ee059ae7688cc217fe5fa793d038d
unknown <joel3.1416@gmail.com>
2021-01-25 10:39:48 -0600

    first 2 problems solved

---
+++ problem0001.py
@@ -0,0 +1,21 @@
+  
+#! python3
+#-*- coding: utf-8 -*-
+
+"""
+Euler description from https://projecteuler.net/
+Problem 0001
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
+3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+"""
+
+#Simple solution
+def compute():
+	ans = sum(x for x in range(1000) if (x % 3 == 0 or x % 5 == 0))
+	return str(ans)
+
+
+if __name__ == "__main__":
+	print(compute())
---
+++ problemTemplate.py
@@ -0,0 +1,18 @@
+  
+#! python3
+#-*- coding: utf-8 -*-
+
+"""
+Euler description from https://projecteuler.net/
+Problem ####
+[Description]
+"""
+
+#main function description
+def compute():
+	ans = sum(x for x in range(1000) if (x % 3 == 0 or x % 5 == 0))
+	return str(ans)
+
+
+if __name__ == "__main__":
+	print(compute())
